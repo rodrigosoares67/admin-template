31d6d7e97fdfda6336388f1bc64d666b71751ba7
Rodrigo Soares <rodrigo.soares67@outlook.com>
2022-07-11 21:55:30 -0300

    FEAT: Criação do menu lateral e páginas principais

---
+++ src/pages/ajustes.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Layout from '../components/template/Layout'
+
+const Ajustes: NextPage = () => {
+  return (
+    <Layout titulo="Ajustes & Configurações" subtitulo="Personalize o sistema por aqui">
+      <h3>Conteúdo</h3>
+    </Layout>
+  )
+}
+
+export default Ajustes
---
+++ src/pages/notificacoes.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Layout from '../components/template/Layout'
+
+const Notificacoes: NextPage = () => {
+  return (
+    <Layout titulo="Notificações" subtitulo="Aqui você irá gerenciar suas notificações">
+      <h3>Conteúdo</h3>
+    </Layout>
+  )
+}
+
+export default Notificacoes
