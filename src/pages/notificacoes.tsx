397ef1ad7740e0f6ebd67452731a553ce14d1942
Rodrigo Soares <rodrigo.soares67@outlook.com>
2022-07-14 23:26:43 -0300

    FEAT: Habilitando funcionalidade para alternar modo Dark e Light

--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,10 +1,13 @@
 import type { NextPage } from 'next'
 import Layout from '../components/template/Layout'
+import useAppData from '../data/hook/useAppData'
 
 const Notificacoes: NextPage = () => {
+  const ctx = useAppData()
+
   return (
     <Layout titulo="Notificações" subtitulo="Aqui você irá gerenciar suas notificações">
-      <h3>Conteúdo</h3>
+      <button onClick={ctx.alternarTema}>Alternar Tema</button>
     </Layout>
   )
 }
