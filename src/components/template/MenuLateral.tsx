2d6ea91be7fede0f499864610088addbf9b3e3eb
Rodrigo Soares <rodrigo.soares67@outlook.com>
2022-08-29 22:34:13 -0300

    FEAT: Implementação do logout

--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,8 +1,12 @@
+import useAuth from "../../data/hook/useAuth";
 import { IconeAjustes, IconeCasa, IconeSair, IconeSino } from "../icons";
 import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function MenuLateral(){
+
+  const { logout } = useAuth()
+
   return (
     <aside className={`
       flex flex-col
@@ -27,7 +31,7 @@ export default function MenuLateral(){
         <MenuItem 
           texto="Sair" 
           icone={IconeSair} 
-          onClick={() => console.log('logout')} 
+          onClick={logout}
           className={`
             text-red-600 dark:text-red-400
             hover:bg-red-400 hover:text-white
